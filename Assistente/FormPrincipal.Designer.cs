7b293218bdf1a8a937595c057d27648507322df5
Paula Calefi <paulaoliveiracalefi@gmail.com>
2020-06-10 00:34:10 -0300

    primeiro chart

--- Assistente/FormPrincipal.Designer.cs
+++ Assistente/FormPrincipal.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation horizontalLineAnnotation1 = new System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
@@ -38,6 +37,11 @@
             this.cbxProjeto = new System.Windows.Forms.ComboBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.LeadTimeChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.rb95 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rb80 = new System.Windows.Forms.RadioButton();
+            this.rb70 = new System.Windows.Forms.RadioButton();
+            this.rb50 = new System.Windows.Forms.RadioButton();
             this.leadTimeBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.LeadTimeChart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.leadTimeBindingSource)).BeginInit();
@@ -73,27 +77,77 @@
             // LeadTimeChart
             // 
             this.LeadTimeChart.AllowDrop = true;
-            horizontalLineAnnotation1.Name = "HorizontalLineAnnotation1";
-            this.LeadTimeChart.Annotations.Add(horizontalLineAnnotation1);
-            chartArea1.Name = "DistribLeadTime";
+            chartArea1.Name = "ChartArea1";
             this.LeadTimeChart.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.LeadTimeChart.Legends.Add(legend1);
             this.LeadTimeChart.Location = new System.Drawing.Point(16, 61);
             this.LeadTimeChart.Name = "LeadTimeChart";
-            this.LeadTimeChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Light;
-            series1.ChartArea = "DistribLeadTime";
+            this.LeadTimeChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
             series1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             series1.LabelAngle = 90;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.LeadTimeChart.Series.Add(series1);
-            this.LeadTimeChart.Size = new System.Drawing.Size(895, 189);
+            this.LeadTimeChart.Size = new System.Drawing.Size(1043, 398);
             this.LeadTimeChart.TabIndex = 3;
             this.LeadTimeChart.Text = "Distribuição de Leadtime";
             title1.Name = "Title1";
             this.LeadTimeChart.Titles.Add(title1);
-            this.LeadTimeChart.UseWaitCursor = true;
+            // 
+            // rb95
+            // 
+            this.rb95.AutoSize = true;
+            this.rb95.Location = new System.Drawing.Point(72, 40);
+            this.rb95.Name = "rb95";
+            this.rb95.Size = new System.Drawing.Size(45, 17);
+            this.rb95.TabIndex = 4;
+            this.rb95.TabStop = true;
+            this.rb95.Text = "95%";
+            this.rb95.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Percentil";
+            // 
+            // rb80
+            // 
+            this.rb80.AutoSize = true;
+            this.rb80.Location = new System.Drawing.Point(123, 40);
+            this.rb80.Name = "rb80";
+            this.rb80.Size = new System.Drawing.Size(45, 17);
+            this.rb80.TabIndex = 6;
+            this.rb80.TabStop = true;
+            this.rb80.Text = "80%";
+            this.rb80.UseVisualStyleBackColor = true;
+            // 
+            // rb70
+            // 
+            this.rb70.AutoSize = true;
+            this.rb70.Location = new System.Drawing.Point(174, 40);
+            this.rb70.Name = "rb70";
+            this.rb70.Size = new System.Drawing.Size(45, 17);
+            this.rb70.TabIndex = 7;
+            this.rb70.TabStop = true;
+            this.rb70.Text = "70%";
+            this.rb70.UseVisualStyleBackColor = true;
+            // 
+            // rb50
+            // 
+            this.rb50.AutoSize = true;
+            this.rb50.Location = new System.Drawing.Point(225, 40);
+            this.rb50.Name = "rb50";
+            this.rb50.Size = new System.Drawing.Size(45, 17);
+            this.rb50.TabIndex = 8;
+            this.rb50.TabStop = true;
+            this.rb50.Text = "50%";
+            this.rb50.UseVisualStyleBackColor = true;
             // 
             // leadTimeBindingSource
             // 
@@ -103,7 +157,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(923, 450);
+            this.ClientSize = new System.Drawing.Size(1071, 450);
+            this.Controls.Add(this.rb50);
+            this.Controls.Add(this.rb70);
+            this.Controls.Add(this.rb80);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rb95);
             this.Controls.Add(this.LeadTimeChart);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.cbxProjeto);
@@ -125,6 +184,11 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.DataVisualization.Charting.Chart LeadTimeChart;
         private System.Windows.Forms.BindingSource leadTimeBindingSource;
+        private System.Windows.Forms.RadioButton rb95;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rb80;
+        private System.Windows.Forms.RadioButton rb70;
+        private System.Windows.Forms.RadioButton rb50;
     }
 }
 
--- Assistente/FormPrincipal.cs
+++ Assistente/FormPrincipal.cs
@@ -70,14 +70,8 @@ namespace Assistente
             string jql = "project = " + projetoSelecionado.Key;
             IEnumerable<Issue> jiraIssues = jira.Issues.GetIssuesFromJqlAsync(jql, 50, 0, token).Result;
             Dictionary<string, int> distribuicao = new Dictionary<string, int>();
-            Dictionary<int, int> Moda = new Dictionary<int, int>();
-
-            this.LeadTimeChart.Series.Clear();
-
-            this.LeadTimeChart.Titles.Add("Total Income");
-
-            Series series = this.LeadTimeChart.Series.Add("Total Income");
-            series.ChartType = SeriesChartType.Column;
+            Dictionary<int, int> Modas = new Dictionary<int, int>();
+           
             int Somatorio = 0;
 
             foreach (var issue in jiraIssues)
@@ -89,11 +83,26 @@ namespace Assistente
                     int dias = (int)DataFim.Subtract(DataInicio).TotalDays;
                     distribuicao.Add(issue.Key.ToString(), dias);
                     Somatorio += dias;
-                    //series.Points.AddXY(issue.Key.ToString(), dias);
+                    if (!Modas.ContainsKey(dias))
+                    {
+                        Modas.Add(dias, 1);
+                    }
+                    else
+                    {
+                        var x = Modas[dias];
+                        Modas.Remove(dias);
+                        Modas.Add(dias, x + 1);
+                    }
                 }
             }
+            double valorPercentil = rb95.Checked ? 0.95 : rb80.Checked ? 0.8 : rb70.Checked ? 0.7 : 0.5;
+            var media = Somatorio / distribuicao.Count;
+            var moda = Modas.FirstOrDefault(x => x.Value == Modas.Values.Max()).Key;
+            var percentil = CalculaPercentil(distribuicao.Values.ToArray(), valorPercentil);
+            PreparaChart(distribuicao, media, moda, percentil, valorPercentil);
+            
         }
-        public double Percentil(int[] sequence, double percentil)
+        public double CalculaPercentil(int[] sequence, double percentil)
         {
             Array.Sort(sequence);
             int N = sequence.Length;
@@ -106,6 +115,38 @@ namespace Assistente
                 int k = (int)n;
                 double d = n - k;
                 return sequence[k - 1] + d * (sequence[k] - sequence[k - 1]);
+            }
+        }
+
+        public void PreparaChart(Dictionary<string, int> distribuicao, int media, int moda, double percentil, double valorPercentil)
+        {
+            this.LeadTimeChart.Series.Clear();
+            this.LeadTimeChart.Titles.Add("Histograma");
+            this.LeadTimeChart.BackHatchStyle = ChartHatchStyle.None;
+            Series leadtimeSerie = this.LeadTimeChart.Series.Add("LeadTime");
+            leadtimeSerie.ChartType = SeriesChartType.Column;
+
+            Series mediaSerie = this.LeadTimeChart.Series.Add("Media: " +media);
+            mediaSerie.ChartType = SeriesChartType.Line;
+            mediaSerie.Color = Color.Red;
+            mediaSerie.BorderWidth = 3;
+
+            Series modaSerie = this.LeadTimeChart.Series.Add("Moda: "+ moda);
+            modaSerie.ChartType = SeriesChartType.Line;
+            modaSerie.Color = Color.Black;
+            modaSerie.BorderWidth = 3;
+
+            Series percentilSerie = this.LeadTimeChart.Series.Add("Percentil " + valorPercentil*10 + "%: " + percentil);
+            percentilSerie.ChartType = SeriesChartType.Line;
+            percentilSerie.Color = Color.Purple;
+            percentilSerie.BorderWidth = 3;
+
+            foreach (var item in distribuicao)
+            {
+                leadtimeSerie.Points.AddXY(item.Key.ToString(), item.Value);
+                mediaSerie.Points.AddXY(item.Key, media);
+                modaSerie.Points.AddXY(item.Key, moda);
+                percentilSerie.Points.AddXY(item.Key, percentil);
             }
         }
     }
